395cddc31677b99c2d1d9fbeaadef2a364a0e332
vionadiva <EMAIL_GITHUB_KAMU@contoh.com>
2026-06-05 13:36:36 +0700

    Website Pertemuan 13

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,23 +2,18 @@
     <x-slot:title>{{ $title }}</x-slot:title>
 
     @foreach ($posts as $post)
+        <article class="py-8 max-w-screen-md border-b border-gray-300">
+            <a href="/blog/{{ $post['slug'] }}" class="hover:underline">
+                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+            </a>
 
-        <article class="mb-5 border-b pb-5">
+            <div class="text-base text-gray-500">
+                <a href="#">{{ $post['author'] }}</a> | 1 January 2024
+            </div>
 
-            <h2 class="text-xl font-bold">
-                {{ $post['title'] }}
-            </h2>
+            <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
 
-            <p>
-                Penulis : {{ $post['author'] }}
-            </p>
-
-            <p>
-                {{ $post['body'] }}
-            </p>
-
+            <a href="/blog/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
         </article>
-
     @endforeach
-
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use Illuminate\Support\Arr;
 
 Route::get('/', function () {
     return view('home', [
@@ -25,5 +26,21 @@ Route::get('/blog', function () {
     return view('blog', [
         'title' => 'Blog',
         'posts' => Post::all()
+    ]);
+});
+
+Route::get('/blog/{slug}', function ($slug) {
+
+    $allPosts = Post::all();
+
+    $post = Arr::first($allPosts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    abort_if(!$post, 404);
+
+    return view('post', [
+        'title' => 'Single Post',
+        'post' => $post
     ]);
 });
